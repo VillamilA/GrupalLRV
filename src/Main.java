ff9c2a4b28d1b38b98ca6ee73dbd152ea60198d0
Angelo <angelo.liguista@gmail.com>
2024-05-29 20:57:26 -0500

    Clase Hija Auto hecha por Angelo Loor

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 // Clase principal para probar la jerarquía
 public class Main {
     public static void main(String[] args) {
-        Vehiculo miVehiculo = new Vehiculo("Genérica", "Model X", 180);
-        System.out.println("Información del Vehículo:");
+        Vehiculo miVehiculo = new Vehiculo("China", "Model X", 180);
+        System.out.println("Informacion del Vehiculo:");
         miVehiculo.mostrarInformacion();
         miVehiculo.acelerar();
+
+        //creamos el objeto y mostramos la información del auto
+        Automovil miAutomovil = new Automovil("Toyota", "Corolla", 220, 4);
+
+        System.out.println("\nInformacion del Auto:");
+        miAutomovil.mostrarInformacion();
+        miAutomovil.acelerar();
     }
 }
--- src/Vehiculos.java
+++ src/Vehiculos.java
@@ -21,3 +21,24 @@ class Vehiculo {
         System.out.println("El vehículo esta acelerando.");
     }
 }
+
+//creamos la clase hija automovil
+//Hecho por Angelo Loor
+class Automovil extends Vehiculo {
+    int numeroDePuertas;
+
+    public Automovil(String marca, String modelo, int velocidadMaxima, int numeroDePuertas) {
+        super(marca, modelo, velocidadMaxima);
+        this.numeroDePuertas = numeroDePuertas;
+    }
+
+    public void mostrarInformacion() {
+        super.mostrarInformacion();
+        System.out.println("Numero de puertas: " + numeroDePuertas);
+    }
+
+
+    public void acelerar() {
+        System.out.println("El automovil está acelerando.");
+    }
+}
